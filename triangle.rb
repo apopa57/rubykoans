05846c6c33cc0bdc3138632380db93fc62c112a2
Popa Alexandru <alexandru.popa@rakuten.com>
2015-10-20 09:33:19 +0900

    Complete level about_triangle_project_2

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,12 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  if a <= 0 || b <= 0 || c <= 0
+    raise TriangleError
+  elsif a >= b+c || b >= a+c || c >= a+b
+    raise TriangleError
+  end
+
   if a == b && b == c
     return :equilateral
   elsif a != b && a != c && b != c
